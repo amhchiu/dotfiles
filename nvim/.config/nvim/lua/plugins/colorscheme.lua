86d8c7feff481c07ba17c431b69f24fdf632994d
amhchiu <24362923+amhchiu@users.noreply.github.com>
2025-05-09 10:43:04 +0100

    add catpuccin nvim theme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -3,10 +3,28 @@ return {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
+    enabled = false,
     opts = { style = "moon", transparent = true },
     config = function(_, opts)
       require("tokyonight").setup(opts)
       vim.cmd.colorscheme "tokyonight"
     end,
+  },
+  { 
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "frappe", -- "latte", "frappe", "macchiato", "mocha"
+        custom_highlights = function(colors)
+            return {
+                Comment = { fg = "#8FBC8F", italic = true },
+            }
+        end
+
+      })
+      vim.cmd.colorscheme "catppuccin"
+    end
   },
 }
