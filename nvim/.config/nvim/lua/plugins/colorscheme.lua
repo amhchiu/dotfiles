2f5a8d62e152f626b69b9b5ebfd13db15c67428e
amhchiu <24362923+amhchiu@users.noreply.github.com>
2025-05-11 18:42:19 +0100

    feat: everforest

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -31,6 +31,7 @@ return {
     'sainnhe/gruvbox-material',
     lazy = false,
     priority = 1000,
+    enabled = false,
     config = function()
       -- Optionally configure and load the colorscheme
       -- directly inside the plugin declaration.
@@ -39,4 +40,15 @@ return {
       vim.cmd.colorscheme('gruvbox-material')
     end
   },
+  {
+    'sainnhe/everforest',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.everforest_enable_italic = true
+      vim.g.everforest_background = 'hard'
+      vim.g.everforest_transparent_background = 1
+      vim.cmd.colorscheme('everforest')
+    end
+  }
 }
